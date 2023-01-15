6b3cfbdbedae15f4642c27ed2b4aabaafc17d7c0
sanjeev40 <sanjeev@worknetwork.in>
2023-01-15 20:01:33 +0530

    Add example files for bundling

---
+++ example/circle.js
@@ -0,0 +1,7 @@
+const PI = 3.14;
+
+function area(radius) {
+  return PI * radius * radius
+}
+
+export default area;
---
+++ example/index.js
@@ -0,0 +1,7 @@
+import message from "./message"
+import areaOfCircle from "./circle"
+import areaOfSquare from "./square"
+
+console.log(message)
+console.log('Area of circle: ', areaOfCircle(10))
+console.log('Area of square: ', areaOfSquare(10))

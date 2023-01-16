21630677739120bccc945f81c9c0b3084d64fc20
sanjeev40 <sanjeev@worknetwork.in>
2023-01-16 19:40:28 +0530

    Create dependency graph from entry file

--- bundler.js
+++ bundler.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
-const babylon = require('babylon')
+const path = require('path')
+const parser = require('@babel/parser')
 const traverse = require('babel-traverse').default
 
 let ID = 0
@@ -7,16 +8,18 @@ let ID = 0
 function createAsset(filename) {
   const content = fs.readFileSync(filename, 'utf-8')
   
-  // Construct an AST(Abstract Syntax Tree) from 
+  // Construct an AST(Abstract Syntax Tree) from
   // the file content.
-  const ast = babylon.parse(content, {sourceType: "module"})
+  const ast = parser.parse(content, {
+    sourceType: "module"
+  })
 
   const dependencies = []
 
   // Collect all the files we are dependent on
   traverse(ast, {
     ImportDeclaration: ({node}) => {
-      dependencies.push(node.source.value)
+      dependencies.push(`${node.source.value}.js`)
     }
   })
 
@@ -30,5 +33,27 @@ function createAsset(filename) {
   }
 }
 
-const mainAsset = createAsset("./example/index.js")
-console.log(mainAsset)
+function createDependencyGraph(entry) {
+  const mainAsset = createAsset(entry)
+  const queue = [mainAsset]
+
+  for (const asset of queue) {
+    const dirname = path.dirname(asset.filename)
+    
+    asset.mapping = {}
+
+    asset.dependencies.forEach(relativePath => {
+      const absolutePath = path.join(dirname, relativePath)
+      const child = createAsset(absolutePath)
+
+      asset.mapping[relativePath] = child.id
+
+      queue.push(child)
+    })
+  }
+
+  return queue
+}
+
+const graph = createDependencyGraph("./example/index.js")
+console.log(graph)
